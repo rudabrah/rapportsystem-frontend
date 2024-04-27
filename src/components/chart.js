c3a7254c4f25d54c0579b16775f5a3be7ccabac3
Rudi Abrahamsen <47385921+rudabrah@users.noreply.github.com>
2024-04-27 21:23:19 +0200

    Update chart.js

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,5 +1,7 @@
 import dynamic from 'next/dynamic';
-import { styled } from '@mui/material/styles';
+//import { styled } from '@mui/material/styles';
+import { styled } from '@mui/system';
+
 
 const ApexChart = dynamic(() => import('react-apexcharts'), {
   ssr: false,
